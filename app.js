e276890f4fde2903d5a1272d6bfd1c3ac4b7c2a7
sehnara <gsh723@naver.com>
2022-06-13 19:51:16 +0900

    upload problems

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const fs = require('fs');
 const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filepath).toString().split("\n")
+const input = fs.readFileSync(filepath).toString()
 
 function solution() {
-    const n = input[0]*1
-    const k = input[1]*1
-    const nums = input.slice(2,2+n).map(i => {return i*1})    
-    const visited = new Array(n).fill(false)
-    const temp = []
-    let result = []
-    function dfs(k){
-        if(k == 0){
-            result.push(temp.join(''))
-            return
-        }
-        for(let i=0; i < n; i++){
-            if(visited[i] === false){
-                visited[i] = true
-                temp.push(nums[i])
-                dfs(k-1)
-                temp.pop()
-                visited[i] = false
-            }
-        }
-    }
+   const n = input *1
+   let a = 1
+   let b = 3
+   let count = 0;
 
-    dfs(k);
-    result = [...new Set(result)]
-    console.log(result.length)
+   function hanio(n,a,b){
+    if(n>1){
+        hanio(n-1, a, 6-a-b) 
+    }
+    console.log(`${a} ${b}`);    
+    if(n>1){
+        hanio(n-1, 6-a-b, b)
+    }
+   }
+   if(n <= 20){
+    hanio(n,a,b)
+   }
 }
 solution()
