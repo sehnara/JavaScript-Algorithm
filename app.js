e1a9cc233a6656cfcaacefb46a1d8959c5f47bec
sehnara <gsh723@naver.com>
2022-08-18 15:52:08 +0900

    백준 : 정규표현식 문제

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-'use strict';
-const fs = require('fs');
-const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filepath).toString().trim().split("\n")
+"use strict";
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filepath).toString().trim().split("\n");
 
 function solution() {
-    let arr = input[0]
-    const croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-    for(let i = 0 ; i < croatia.length ; i++){
-       arr = arr.split(croatia[i]).join("A")
+  input.map((e) => {
+    while(e.includes('BUG')){
+        e = e.replace(/BUG/g, "");
     }
-    console.log(arr.length)
-
+    console.log(e);
+  });
 }
-solution()
+solution();
---
+++ 백준/3447_버그왕/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filepath).toString();
+function solution() {
+  console.log(input);
+}
+solution();
